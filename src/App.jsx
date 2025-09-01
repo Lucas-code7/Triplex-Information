e201b3b65a5545a3a6a8ffb47dfcdfe209cea045
Lucas <lucasflorez956@gmail.com>
2025-09-01 13:54:46 -0300

    proyecto arreglado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import NavBar from './componentes/navBar'
+import NavBar from './componentes/NavBar'
 import ItemListContainer from './componentes/ItemListContainer'
 
 function App() {
